5a9630feef0de28197a798460365fa6e724c7301
go-ai-agent <mark.wilsonai@outlook.com>
2023-10-08 05:11:48 -0500

    Update entry.go

--- timeseries/entry.go
+++ timeseries/entry.go
@@ -15,18 +15,18 @@ type Entry struct {
 	Service    string
 	InstanceId string
 
-	Uri string // {scheme}://{host}/{path}
-	// How to determine if uri is primary or secondary, should use traffic percentage
+	Uri         string // {scheme}://{host}/{path}
+	RouteName   string // primary|secondary
 	Method      string
 	StatusCode  int32
 	StatusFlags string
 
 	// Needed to verify client controller configuration matches configuration in cloud
 	// Can this be replaced with a periodic audit?
-	Timeout           int32
-	RateLimit         float64
-	RateBurst         int32
-	TrafficPercentage int32
-	// Proxy methods  ??
+	Timeout   int32
+	RateLimit float64
+	RateBurst int32
+	RoutePct  int32 // A -1 value is for a disabled controller, everything else is the percentage of
+	// traffic routed to secondary
 
 }
