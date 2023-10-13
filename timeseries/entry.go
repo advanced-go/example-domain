ce3b876142d8d47dd61ec3aa1ec9a9e88934bee5
go-ai-agent <mark.wilsonai@outlook.com>
2023-10-13 06:58:39 -0500

    Update entry.go

--- timeseries/entry.go
+++ timeseries/entry.go
@@ -2,21 +2,46 @@ package timeseries
 
 import "time"
 
-// Entry - timeseries struct
-type Entry struct {
-	Traffic        string
-	Start          time.Time
-	Duration       time.Duration
-	ControllerName string
+// Research - https://www.faa.gov/nextgen
 
+// Entry - timeseries struct provides support for the following:
+//      1. Resiliency - Comparing real time metrics against controller attributes and making appropriate
+//                      controller configuration changes.
+//      2. Traffic Analytics - Provide real time diagnostics of a distributed ecosystem, and affect
+//                                      appropriate changes to keep ecosystem resilient and highly available.
+//                                      Can routing be changed to avoid failures?
+//      3. Traffic Triage - Analyze routing from the perspective of a request ID. Provide answers to the
+//                          following questions:
+//                          When, where, why did a request fail?
+//                          Can this kind of failure be mitigated?
+//                          Are multiple requests related?
+//
+// Notes:
+
+type Entry struct {
+	Traffic    string
+	Start      time.Time
+	Duration   time.Duration
+	Controller string
+
+	// Do we need an Origin Uri ?? A concatenation of the following fields. Maybe a controller Uri??
 	Region     string
 	Zone       string
 	SubZone    string
 	Service    string
 	InstanceId string
 
-	Uri         string // {scheme}://{host}/{path}
+	// Header field attributes
+	// Anymore use cases??
+	RequestId string
+	RelatesTo string // Allows further segmentation of requests by topic/entity such as a customer ID
+
+	// Request attributes
+	Url         string // {scheme}://{host}/{path} No query
 	Route       string // primary|secondary
+	Protocol    string // From timeseries
+	Host        string // From timeseries
+	Path        string // From timeseries
 	Method      string
 	StatusCode  int32
 	StatusFlags string
