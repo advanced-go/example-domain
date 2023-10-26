0b61d63a06e72c83ddc3dda523c540accb47a990
go-ai-agent <mark.wilsonai@outlook.com>
2023-10-26 09:26:05 -0500

    Update z_package_test.go

--- google/z_package_test.go
+++ google/z_package_test.go
@@ -1,1 +1,55 @@
 package google
+
+import (
+	"github.com/go-ai-agent/core/runtime"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestDoHandler(t *testing.T) {
+	type args struct {
+		req *http.Request
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *http.Response
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := DoHandler(tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DoHandler() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DoHandler() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_searchHandler(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+		want *runtime.Status
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchHandler[runtime.DebugError](tt.args.w, tt.args.r); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchHandler() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
