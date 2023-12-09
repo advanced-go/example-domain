93df2e750dddc20c4185607f9daa82551d517824
go-ai-agent <mark.wilsonai@outlook.com>
2023-12-09 11:30:27 -0600

    Update get_test.go

--- activity/get_test.go
+++ activity/get_test.go
@@ -1,43 +1,8 @@
 package activity
 
 import (
-	"errors"
 	"fmt"
-	"github.com/advanced-go/core/io2"
-	"github.com/advanced-go/core/json2"
-	"github.com/advanced-go/core/runtime"
-	"net/http"
-	"net/url"
 )
-
-func getProxy(h http.Header, uri *url.URL) (any, runtime.Status) {
-	content := h.Get(ContentLocation)
-	if len(content) == 0 {
-		return nil, runtime.NewStatusError(runtime.StatusInvalidContent, "getProxy", errors.New("content-location is empty"))
-	}
-	u, _ := url.Parse(content)
-	buf, err := io2.ReadFile(u)
-	if err != nil {
-		return nil, runtime.NewStatusError(runtime.StatusInvalidContent, "getProxy", err)
-	}
-	var entries []Entry
-	status := json2.Unmarshal(buf, &entries)
-	return entries, status
-}
-
-func _Example_getEntryHandler() {
-	ctx := runtime.NewProxyContext(nil, getProxy)
-	h := make(http.Header)
-	h.Add(ContentLocation, "file://[cwd]/activitytest/resource/activity-entry-v1.json")
-	u, _ := url.Parse("http://advanced-go/example-domain/activity/entry")
-
-	entries, status := getEntryHandler[runtime.Output](ctx, h, u)
-	fmt.Printf("test: getEntryHandler() -> [entries:%v] [status:%v]\n", entries, status)
-
-	//Output:
-	//test
-
-}
 
 func Example_getEntryFromPath() {
 	location := "file://[cwd]/activitytest/resource/activity-entry-v1.json"
