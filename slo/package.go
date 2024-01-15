06f025713f0fa0a53f67765e46af5d0162817c86
go-ai-agent <mark.wilsonai@outlook.com>
2024-01-15 12:13:00 -0600

    Updating

--- slo/package.go
+++ slo/package.go
@@ -34,32 +34,3 @@ func PostEntry[T PostEntryConstraints](h http.Header, method string, values url.
 	defer access.LogDeferred(access.InternalTraffic, access.NewRequest(h, method, postEntryLoc), postRouteName, "", -1, "", &status)()
 	return postEntryHandler[runtime.Log](nil, h, method, values, body)
 }
-
-/*
-// HttpHandler - http endpoint
-func HttpHandler(w http.ResponseWriter, r *http.Request) {
-	if r == nil {
-		http2.WriteResponse[runtime.Log](w, nil, runtime.NewStatus(runtime.StatusInvalidArgument), nil)
-		return
-	}
-	_, rsc, ok := uri.UprootUrn(r.URL.Path)
-	if !ok || len(rsc) == 0 {
-		status := runtime.NewStatusWithContent(http.StatusBadRequest, errors.New(fmt.Sprintf("error invalid path, not a valid URN: %v", r.URL.Path)), false)
-		http2.WriteResponse[runtime.Log](w, nil, status, nil)
-		return
-	}
-	runtime.AddRequestId(r)
-	switch strings.ToLower(rsc) {
-	case entryResource:
-		//func() (status runtime.Status) {
-		//	defer access.LogDeferred(access.InternalTraffic, r, httpHandlerRouteName, "", -1, "", &status)()
-		httpEntryHandler[runtime.Log](w, r)
-		//}()
-	default:
-		status := runtime.NewStatusWithContent(http.StatusNotFound, errors.New(fmt.Sprintf("error invalid URI, resource was not found: %v", rsc)), false)
-		http2.WriteResponse[runtime.Log](w, nil, status, nil)
-	}
-}
-
-
-*/
